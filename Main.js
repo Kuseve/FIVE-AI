ce5a5494f1215abd62777f090a19dc39341c2ae6
severrabaen <39644970+severrabaen@users.noreply.github.com>
2018-09-21 11:22:43 +0900

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,41 @@
+// Electron側の初期設定
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+// アプリを閉じた時にquit
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
+// アプリ起動後の処理
+app.on('ready', function() {
+  var subpy = require('child_process').spawn('python',['./hello.py']);
+  var rq = require('request-promise');
+  var mainAddr = 'http://localhost:5000';
+
+  var openWindow = function() {
+    mainWindow = new BrowserWindow({width: 400, height: 300 });
+    mainWindow.loadURL(mainAddr);
+
+    // 終了処理
+    mainWindow.on('closed', function() {
+      mainWindow = null;
+      subpy.kill('SIGINT');
+    });
+  };
+
+  var startUp = function() {
+    rq(mainAddr)
+      .then(function(htmlString) {
+        console.log('server started');
+        openWindow();
+      })
+      .catch(function(err) {
+        startUp();
+      });
+  };
+
+  startUp();
+});
