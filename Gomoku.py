81e5bc3df880c8fa335b28e957277d27dcda219e
severrabaen <39644970+severrabaen@users.noreply.github.com>
2018-10-04 19:15:28 +0900

    Update Gomoku.py

--- Gomoku.py
+++ Gomoku.py
@@ -9,6 +9,20 @@ from tkinter import *
 #後でファイルは分ける
 
 
+class fullScreen(object): 
+    def __init__(self, master, **kwargs): 
+     self.master=master 
+     pad=3 
+     self._geom='200x200+0+0' 
+     master.geometry("{0}x{1}+0+0".format(
+      master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad)) 
+     master.bind('<Escape>',self.toggle_geom)    
+    def toggle_geom(self,event): 
+     geom=self.master.winfo_geometry() 
+     print(geom,self._geom) 
+     self.master.geometry(self._geom) 
+     self._geom=geom 
+
 #マスは15*15
 grid_height=15
 grid_width=15
@@ -17,6 +31,7 @@ score=0
 #メイン画面
 root = Tk()
 root.title("FIVE AI")
+app=fullScreenApp(root) 
 root.resizable(0,0)     #サイズ変更不可にする
  
 root.mainloop()
