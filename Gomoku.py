3c4b069199f80a44b3289320f1ad2ab5c88cd4fa
Kuske <lucky.777yusuke@gmail.com>
2018-10-16 21:51:33 +0900

    [Gomoku.py, Main.py] インデントをきれいにした

--- Gomoku.py
+++ Gomoku.py
@@ -6,69 +6,78 @@ import dropbox
 from flask import Flask
 from tkinter import *
 
-#後でファイルは分ける
+# 後でファイルは分ける
 
 
-class fullScreen(object): 
-    def __init__(self, master, **kwargs): 
-     self.master=master 
-     pad=3 
-     self._geom='200x200+0+0' 
-     master.geometry("{0}x{1}+0+0".format(
-      master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad)) 
-     master.bind('<Escape>',self.toggle_geom)    
-    def toggle_geom(self,event): 
-     geom=self.master.winfo_geometry() 
-     print(geom,self._geom) 
-     self.master.geometry(self._geom) 
-     self._geom=geom 
+class fullScreen(object):
+    def __init__(self, master, **kwargs):
+        self.master = master
+        pad = 3
+        self._geom = '200x200+0+0'
+        master.geometry("{0}x{1}+0+0".format(
+            master.winfo_screenwidth() - pad, master.winfo_screenheight() - pad))
+        master.bind('<Escape>', self.toggle_geom)
 
-#マスは15*15
-grid_height=15
-grid_width=15
-score=0
+    def toggle_geom(self, event):
+        geom = self.master.winfo_geometry()
+        print(geom, self._geom)
+        self.master.geometry(self._geom)
+        self._geom = geom
 
-#メイン画面
+
+# マスは15*15
+grid_height = 15
+grid_width = 15
+score = 0
+
+# メイン画面
 root = Tk()
 root.title("FIVE AI")
-app=fullScreenApp(root) 
-root.resizable(0,0)     #サイズ変更不可にする
- 
+app = fullScreenApp(root)
+root.resizable(0, 0)  # サイズ変更不可にする
+
 root.mainloop()
 
-#左クリックされた時の処理
+# 左クリックされた時の処理
+
+
 def leftClicked():
-  
-  
-#マス目描画
+
+
+    # マス目描画
 i = 0
 frame_list = []
 for x in range(grid_height):
     for y in range(grid_width):
-        frame = Frame(game_frame, width = 30, height = 30, bd = 3, relief = 'raised', bg = 'LightGray')
+        frame = Frame(game_frame, width=30, height=30,
+                      bd=3, relief='raised', bg='LightGray')
         frame.bind("<1>", leftClicked)
         frame.num = i
         frame_list.append(frame)
         frame.grid(row=x, column=y)
         i += 1
 
-#ドロボ(からファイルを持ってくる)
+# ドロボ(からファイルを持ってくる)
+
+
 def fromDropbox():
-  global score
-  dbox=dropbox.Dropbox(os.environ["DROPBOX_KEY"])
-  dbox.users_get_current_account()
-  
-#ドロボ(にファイルを入れる)
+    global score
+    dbox = dropbox.Dropbox(os.environ["DROPBOX_KEY"])
+    dbox.users_get_current_account()
+
+# ドロボ(にファイルを入れる)
+
+
 def intoDropbox():
-  global score
-  dbox = dropbox.Dropbox(os.environ["DROPBOX_KEY"])
-  dbox.users_get_current_account()
-  
-  
+    global score
+    dbox = dropbox.Dropbox(os.environ["DROPBOX_KEY"])
+    dbox.users_get_current_account()
+
+
 def processing():
-  #ここでいろいろな処理をする
-  
-  
+    # ここでいろいろな処理をする
+
+
 def ranking():
-  #ドロボを使用してランキングを作りたい
-  fromDropbox()
+    # ドロボを使用してランキングを作りたい
+    fromDropbox()
--- Main.py
+++ Main.py
@@ -1,63 +1,65 @@
 from tkinter import *
 import numpy as np
 
-#ここら辺はグローバル変数とか定数とか
-#マスは15*15
-grid_height=15
-grid_width=15
-#手数記録用
-movedcount=0
-#既に駒が置かれているかどうか
-emporfull=np.array
+# ここら辺はグローバル変数とか定数とか
+# マスは15*15
+grid_height = 15
+grid_width = 15
+# 手数記録用
+movedcount = 0
+# 既に駒が置かれているかどうか
+emporfull = np.array
 
-#class fullScreen(object): 
-   # def __init__(self, master, **kwargs): 
-     #self.master=master 
-     #pad=3 
-     #self._geom='200x200+0+0' 
-     #master.geometry("{0}x{1}+0+0".format(
-     #master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad)) 
-     #master.bind('<Escape>',self.toggle_geom)    
-     #def toggle_geom(self,event): 
-      # geom=self.master.winfo_geometry() 
-       #print(geom,self._geom) 
-       #self.master.geometry(self._geom) 
-       #self._geom=geom 
-      
-#メイン画面
+# class fullScreen(object):
+# def __init__(self, master, **kwargs):
+# self.master=master
+# pad=3
+# self._geom='200x200+0+0'
+# master.geometry("{0}x{1}+0+0".format(
+# master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
+# master.bind('<Escape>',self.toggle_geom)
+# def toggle_geom(self,event):
+# geom=self.master.winfo_geometry()
+# print(geom,self._geom)
+# self.master.geometry(self._geom)
+# self._geom=geom
+
+# メイン画面
 root = Tk()
 root.title("FIVE AI")
-#app=fullScreen(root) 
-root.resizable(0,0)
-      
-#メニュー
+# app=fullScreen(root)
+root.resizable(0, 0)
+
+# メニュー
 menu_ROOT = Menu(root)
-root.configure(menu = menu_ROOT)
-menu_GAME = Menu(menu_ROOT, tearoff = False)
-menu_ROOT.add_cascade(label = 'ゲーム', under = 4, menu = menu_GAME)
+root.configure(menu=menu_ROOT)
+menu_GAME = Menu(menu_ROOT, tearoff=False)
+menu_ROOT.add_cascade(label='ゲーム', under=4, menu=menu_GAME)
 
-menu_GAME.add_command(label = "かんたん", under = 3)
-menu_GAME.add_command(label = "ふつう", under = 3)
-menu_GAME.add_command(label = "むずかしい", under = 3)
-menu_GAME.add_command(label = "Ω", under = 3)
+menu_GAME.add_command(label="かんたん", under=3)
+menu_GAME.add_command(label="ふつう", under=3)
+menu_GAME.add_command(label="むずかしい", under=3)
+menu_GAME.add_command(label="Ω", under=3)
 
-menu_ROOT.add_command(label = "終了", under = 3)
+menu_ROOT.add_command(label="終了", under=3)
 
-#ゲーム画面配置
-root_frame = Frame(root, relief = 'groove', borderwidth = 5, bg = 'LightGray')
-game_frame = Frame(root_frame, width = 300, height = 300, relief = 'ridge', borderwidth = 3, bg = 'LightGreen')
+# ゲーム画面配置
+root_frame = Frame(root, relief='groove', borderwidth=5, bg='LightGray')
+game_frame = Frame(root_frame, width=300, height=300,
+                   relief='ridge', borderwidth=3, bg='LightGreen')
 root_frame.pack()
-game_frame.pack(pady = 5, padx = 5)
-  #マス目描画
+game_frame.pack(pady=5, padx=5)
+# マス目描画
 i = 0
 frame_list = []
 for x in range(grid_height):
     for y in range(grid_width):
-        frame = Frame(game_frame, width = 30, height = 30, bd = 3, relief = 'raised', bg = 'LightGray')
+        frame = Frame(game_frame, width=30, height=30,
+                      bd=3, relief='raised', bg='LightGray')
         #frame.bind("<1>", leftClicked)
         frame.num = i
         frame_list.append(frame)
         frame.grid(row=x, column=y)
         i += 1
-        
+
 root.mainloop()
