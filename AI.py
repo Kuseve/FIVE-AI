ab08b49bacfa2c5eddcdb0659e9ddc4bfcfa34ea
Kuske <lucky.777yusuke@gmail.com>
2018-12-16 22:30:52 +0900

    weakAI()の簡単な実装

--- AI.py
+++ AI.py
@@ -1,2 +1,16 @@
+from numpy.random import *
 import numpy as np
-import tensorflow as tf
+from Main import *
+
+# AIのコマンド
+def weakAI():
+    return Point(randint(14), randint(14))
+
+def middleAI():
+    return 0
+
+def strongAI():
+    return 0
+
+def omgAI():
+    return 0
