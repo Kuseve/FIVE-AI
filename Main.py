f3679f902c6746b4700974a4131513bb54164e98
severrabaen <39644970+severrabaen@users.noreply.github.com>
2018-12-18 19:31:54 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,35 +9,8 @@ from flask import Flask
 from AI import *
 import define
 
-# 構造体の宣言
-class Size:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-# デバッグモードのフラグ
-isDebug = False
-
 #マス目を格納する配列
 frame_list = []
-
-# 0=empty,1=player,2=AI
-class gridState(IntEnum):
-    empty = 0
-    player = auto()
-    AI = auto()
-
-# AIの強さ
-class AILevel(IntEnum):
-    week = 0
-    middle = auto()
-    strong = auto()
-    omg = auto()
 
 # 戦うモード
 class gameMode(IntEnum):
